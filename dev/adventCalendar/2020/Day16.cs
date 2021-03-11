5cad448d491ac47ca7132849547257160269864f
Antoine <coulombe.antoine@hotmail.com>
2021-03-11 17:36:54 -0500

    Refactored Day16 for part 2

--- dev/adventCalendar/2020/Day16.cs
+++ dev/adventCalendar/2020/Day16.cs
@@ -7,15 +7,97 @@ namespace dev.adventCalendar._2020
 {
     class Day16 : Day
     {
-        private class ValidNumbers
+        private class Tickets
         {
+            private List<Constraint> constraints;
+            private List<int> myTicketNumbers;
+            private List<List<int>> nearbyTicketNumbers;
             private bool[] valid;
 
-            private void InitNumbers()
+            public Tickets(List<string> lines)
+            {
+                int myTicketLine = lines.FindIndex(x => x.Contains("your ticket")) + 1;
+                constraints = GetConstraints(lines.Take(myTicketLine - 2).ToList());
+                myTicketNumbers = GetTicketNumbers(lines[myTicketLine]);
+                nearbyTicketNumbers = GetNearbyNumbers(lines.Skip(myTicketLine + 3).ToList());
+                valid = GetValidNumbers();
+            }
+
+            private List<int> GetTicketNumbers(string line)
+                => line.Split(',').Select(int.Parse).ToList();
+
+            private List<List<int>> GetNearbyNumbers(List<string> lines)
+            {
+                var numbers = new List<List<int>>();
+                foreach (string l in lines)
+                    numbers.Add(GetTicketNumbers(l));
+                return numbers;
+            }
+
+            private List<Constraint> GetConstraints(List<string> lines)
+            {
+                var constraints = new List<Constraint>();
+
+                foreach (string l in lines)
+                    if (l.Contains(':'))
+                        constraints.Add(new Constraint(l));
+
+                return constraints;
+            }
+
+            private bool[] GetValidNumbers()
             {
                 valid = new bool[1000];
-                for (int i = 0; i < valid.Length; ++i)
-                    valid[i] = false;
+                foreach (var constraint in constraints)
+                    foreach (var (lowerbound, upperbound) in constraint.ranges)
+                        for (int i = lowerbound; i <= upperbound; ++i)
+                            valid[i] = true;
+                return valid;
+            }
+
+            private void FilterNearbyTickets()
+                => nearbyTicketNumbers.RemoveAll(x => x.Exists(y => !valid[y]));
+
+            private void FindConstraintsValue()
+            {
+
+            }
+
+            public int SumErrorRate()
+            {
+                int total = 0;
+                foreach (var ticket in nearbyTicketNumbers)
+                    foreach (var n in ticket)
+                        if (!valid[n])
+                            total += n;
+                return total;
+            }
+
+            public int MultiplyConstraintsValue(int count)
+            {
+                int total = 1;
+                for (int i = 0; i < count; ++i)
+                    total *= constraints[i].value;
+                return total;
+            }
+        }
+
+        private class Constraint
+        {
+            public readonly string name;
+            public readonly List<(int lowerbound, int upperbound)> ranges;
+            public int value;
+
+            public Constraint(string line)
+            {
+                ranges = new List<(int, int)>();
+
+                var split = line.Split(": ");
+                name = split[0];
+
+                split = split[1].Split(" or ");
+                ranges.Add(SplitNumbers(split[0]));
+                ranges.Add(SplitNumbers(split[1]));
             }
 
             private (int, int) SplitNumbers(string s)
@@ -24,70 +106,25 @@ namespace dev.adventCalendar._2020
                 return (int.Parse(n[0]), int.Parse(n[1]));
             }
 
-            private List<(int, int)> GetNumberRanges(List<string> lines)
+            public bool InRanges(int n)
             {
-                var valid = new List<(int, int)>();
-                var constraintsEnd = lines.FindIndex(x => x.Contains("your ticket")) - 1;
-
-                for (int i = 0; i < constraintsEnd; ++i)
-                {
-                    if (!lines[i].Contains(':'))
-                        continue;
-
-                    var s = lines[i].Split(": ")[1].Split(" or ");
-                    valid.Add(SplitNumbers(s[0]));
-                    valid.Add(SplitNumbers(s[1]));
-                }
-
-                return valid;
+                foreach (var (lowerbound, upperbound) in ranges)
+                    if (n >= lowerbound && n <= upperbound)
+                        return true;
+                return false;
             }
-
-            private void GetValidNumbers(List<(int, int)> numbers)
-            {
-                foreach ((int lower, int upper) in numbers)
-                    for (int i = lower; i <= upper; ++i)
-                        valid[i] = true;
-            }
-
-            public ValidNumbers(List<string> lines)
-            {
-                InitNumbers();
-                GetValidNumbers(GetNumberRanges(lines));
-            }
-
-            public bool[] Get()
-                => valid;
-        }
-
-        private int SumErrorRate(List<string> lines, bool[] validNumbers)
-        {
-            int total = 0;
-
-            int start = lines.FindIndex(x => x.Contains("nearby tickets")) + 1;
-            for (int i = start; i < lines.Count; ++i)
-            {
-                var numbers = lines[i].Split(',');
-                foreach (var num in numbers)
-                {
-                    var n = int.Parse(num);
-                    if (!validNumbers[n])
-                        total += n;
-                }
-            }
-
-            return total;
         }
 
         public override string ExecuteFirst()
         {
-            var lines = GetFileLines(16).ToList();
-            var validNumbers = new ValidNumbers(lines).Get();
-            return SumErrorRate(lines, validNumbers).ToString();
+            var tickets = new Tickets(GetFileLines(16).ToList());
+            return tickets.SumErrorRate().ToString();
         }
 
         public override string ExecuteSecond()
         {
-            return "";
+            var tickets = new Tickets(GetFileLines(16).ToList());
+            return tickets.MultiplyConstraintsValue(6).ToString();
         }
     }
 }
