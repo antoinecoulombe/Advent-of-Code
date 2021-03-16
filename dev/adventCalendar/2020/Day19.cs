a454a01708d3d61cbe878b29e34a7b83738404b3
Antoine <coulombe.antoine@hotmail.com>
2021-03-15 23:52:28 -0400

    Continued Day19.1

--- dev/adventCalendar/2020/Day19.cs
+++ dev/adventCalendar/2020/Day19.cs
@@ -42,13 +42,14 @@ namespace dev.adventCalendar._2020
 
         private class Rules
         {
+            private enum LETTERS { a = 200, b = 201 };
             private List<Rule> rules;
-            private List<bool[]> boolMatches;
-            private List<List<byte>> matches;
+            private List<string> matches;
 
             public Rules(List<string> ruleLines)
             {
                 GetRules(ruleLines);
+                ReplaceNumbersWithLetters();
                 FindMatches();
             }
 
@@ -85,33 +86,91 @@ namespace dev.adventCalendar._2020
                 WriteToFile(lines, 19);
             }
 
+            private (byte a, byte b) GetLetterNumbers()
+            {
+                byte a = byte.MaxValue, b = byte.MaxValue;
+                foreach (Rule r in rules)
+                {
+                    if (r.letter == 'a')
+                        a = r.number;
+                    if (r.letter == 'b')
+                        b = r.number;
+                }
+
+                rules.RemoveAll(x => x.letter == 'a' || x.letter == 'b');
+                return (a, b);
+            }
+
+            private void ReplaceNumbersWithLetters()
+            {
+                var (a, b) = GetLetterNumbers();
+                foreach (Rule r in rules)
+                {
+                    foreach (var s in r.subrules)
+                    {
+                        for (int i = 0; i < s.Count; ++i)
+                        {
+                            if (s[i] == a)
+                                s[i] = (byte)LETTERS.a;
+                            else if (s[i] == b)
+                                s[i] = (byte)LETTERS.b;
+                        }
+                    }
+                }
+            }
+
             private void FindMatches()
             {
-                boolMatches = new List<bool[]>();
+                matches = new();
+                List<List<byte>> poss = new();
+                poss.AddRange(rules.Find(x => x.number == 0).subrules);
 
-                do
+                while (poss.Count > 0)
                 {
+                    string str = "";
+                    var first = poss.First();
+                    while (first.Count > 0)
+                    {
+                        while (IsLetter(first.First()))
+                        {
+                            str += first.First() == (byte)LETTERS.a ? "a" : "b";
+                            first.RemoveAt(0);
 
-                } while (matches.Exists(x => x.Exists(y => y != 200 && y != 201))); // 200 = a, 201 = b
+                            if (first.Count == 0)
+                                break;
+                        }
+
+                        if (first.Count == 0)
+                            break;
+
+                        byte n = first.First();
+                        var newRule = rules.Find(x => x.number == n);
+
+                        first.RemoveAt(0);
+
+                        if (newRule.subrules.Count > 1)
+                        {
+                            poss.Add(new(first));
+                            poss.Last().InsertRange(0, newRule.subrules[1]);
+                        }
+
+                        first.InsertRange(0, newRule.subrules[0]);
+                    }
+
+                    poss.RemoveAt(0);
+                    if (str.Length >= 24 && !matches.Contains(str))
+                    {
+                        matches.Add(str);
+                        Console.WriteLine(str);
+                    }
+                }
             }
 
-            private bool[] ConvertMessage(string msg)
-            {
-                bool[] boolMsg = new bool[msg.Length];
-                for (int i = 0; i < msg.Length; ++i)
-                    boolMsg[i] = msg[i] == 'b';
-                return boolMsg;
-            }
+            private bool IsLetter(byte b)
+                => b == (byte)LETTERS.a || b == (byte)LETTERS.b;
 
-            private bool IsExactMatch(bool[] msg)
-                => boolMatches.Contains(msg);
-
-            public List<bool[]> GetMatches(List<string> msgs)
-            {
-                List<bool[]> boolMsgs = new();
-                msgs.ForEach(x => boolMsgs.Add(ConvertMessage(x)));
-                return boolMsgs.FindAll(x => IsExactMatch(x));
-            }
+            public List<string> GetMatches(List<string> msgs)
+                => msgs.FindAll(x => matches.Contains(x));
         }
 
         private (List<string>, List<string>) GetLines()
@@ -125,7 +184,9 @@ namespace dev.adventCalendar._2020
         public override string ExecuteFirst()
         {
             var (ruleLines, messages) = GetLines();
-            return new Rules(ruleLines).GetMatches(messages).Count.ToString();
+            var rules = new Rules(ruleLines);
+            var matches = rules.GetMatches(messages);
+            return matches.Count.ToString();
         }
 
         public override string ExecuteSecond()
