15ca471cf11cba9eaed82874bf834395083715ee
Antoine <coulombe.antoine@hotmail.com>
2021-05-19 21:51:09 -0400

    Finished Day 2015.08 + Started Day 2015.09

--- dev/Program.cs
+++ dev/Program.cs
@@ -19,7 +19,7 @@ namespace dev
         static void Main(string[] args)
         {
             Console.Clear();
-            AdventCalendar.ExecuteDay(7, 15);
+            AdventCalendar.ExecuteDay(9, 15);
             Console.ReadKey();
         }
     }
--- dev/adventCalendar/2015/Day08.cs
+++ dev/adventCalendar/2015/Day08.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace dev.adventCalendar._2015
 {
@@ -8,12 +10,17 @@ namespace dev.adventCalendar._2015
     {
         public override string ExecuteFirst()
         {
-            return "";
+            var l = GetFileLines(8, 15);
+            return l.Sum(s => s.Length - Regex.Replace(
+                s[1..^1].Replace("\\\"", "A").Replace("\\\\", "B"),
+                "\\\\x[a-f0-9]{2}", "C").Length).ToString();
         }
 
         public override string ExecuteSecond()
         {
-            return "";
+            var l = GetFileLines(8, 15);
+            return l.Sum(s => s.Replace("\\", "AA")
+                .Replace("\"", "BB").Length + 2 - s.Length).ToString();
         }
     }
 }
