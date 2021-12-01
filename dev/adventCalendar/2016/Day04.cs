7a5bafc5258ab3b2fa71ebc197d2c07d8158a0ca
Antoine <coulombe.antoine@hotmail.com>
2021-12-01 18:52:15 -0500

    Finished Day 2016.03 + 2016.04

--- dev/adventCalendar/2016/Day04.cs
+++ dev/adventCalendar/2016/Day04.cs
@@ -1,19 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace dev.adventCalendar._2016
 {
-    class Day04 : Day
+  class Day04 : Day
+  {
+    private (string name, int id)[] GetValidRooms()
     {
-        public override string ExecuteFirst()
-        {
-            return "";
-        }
+      var lines = GetFileLines();
+      List<(string name, int id)> rooms = new();
 
-        public override string ExecuteSecond()
-        {
-            return "";
-        }
+      foreach (var l in lines)
+      {
+        var name = l.Substring(0, l.LastIndexOf('-'));
+        var id = l.Substring(l.LastIndexOf('-') + 1, l.IndexOf('[') - l.LastIndexOf('-') - 1);
+        var checksum = l.Substring(l.IndexOf('[') + 1, l.IndexOf(']') - l.IndexOf('[') - 1);
+
+        var grouped =
+            name.Replace("-", string.Empty)
+            .GroupBy(c => c)
+            .Select(x => (x.Key, x.Count()))
+            .OrderByDescending(x => x.Item2)
+            .ThenBy(x => x.Key).ToList();
+
+        if (String.Join("", grouped.Select(x => x.Key)).StartsWith(checksum))
+          rooms.Add((name, int.Parse(id)));
+      }
+      return rooms.ToArray();
     }
+
+    public override string ExecuteFirst()
+        => GetValidRooms().Sum(x => x.id).ToString();
+
+    public override string ExecuteSecond()
+    {
+      var rooms = GetValidRooms();
+      foreach (var r in rooms)
+      {
+        string s = "";
+        foreach (var c in r.name)
+          s += c == '-' ? " " : (char)(((int)c - 97 + r.id) % 26 + 97);
+
+        if (s.Contains("northpole"))
+          return r.id.ToString();
+      }
+
+      return "Can't find encrypted room.";
+    }
+  }
 }
